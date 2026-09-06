afb511c754d9ea3742295262b68c904f708f9348
freexiaochuan <w2239559319@myyahoo.com>
2026-09-06 13:41:54 +0800

    u

--- test.py
+++ test.py
@@ -1,51 +1,17 @@
-import json
 from pathlib import Path
-from PIL import Image
-from pix2text import TextFormulaOCR
-from .const import imgs_dir
+from src.download_utils import download_zip
+from paddleocr import PaddleOCRVL
 
 
-def recognize_formulas(pdfname, batch_size=64):
-    formulas_dir = Path(imgs_dir) / Path(pdfname).stem / "formulas"
+download_zip()
+output_dir = Path("./output")
+output_dir.mkdir(parents=True, exist_ok=True)
 
-    img_paths = sorted(formulas_dir.glob("*.png"))
+pipeline = PaddleOCRVL()
 
-    if not img_paths:
-        print("没有找到公式图片")
-        return
-
-    ocr = TextFormulaOCR.from_config(device="cuda")
-
-    images = [
-        Image.open(path).convert("RGB")
-        for path in img_paths
-    ]
-
-    print(f"共 {len(images)} 个公式，开始识别...")
-
-    results = ocr.recognize_formula(
-        images,
-        batch_size=batch_size,
-        return_text=True,
-    )
-
-    data = {
-        path.name: latex
-        for path, latex in zip(img_paths, results)
-    }
-
-    output_path = formulas_dir / "formulas.json"
-
-    with open(output_path, "w", encoding="utf-8") as f:
-        json.dump(data, f, ensure_ascii=False, indent=2)
-
-    print(f"识别完成: {output_path}")
-
-    return data
-
-
-if __name__ == "__main__":
-    recognize_formulas(
-        "a.pdf",
-        batch_size=1,
-    )
+output = pipeline.predict("./imgs/a/formulas/00000.png")
+for res in output:
+    res.print() ## 打印预测的结构化输出
+    res.save_to_json(save_path=output_dir) ## 保存当前图像的结构化json结果
+    res.save_to_markdown(save_path=output_dir) ## 保存当前图像的markdown格式的结果
+    res.save_to_word(save_path=output_dir) ## 保存当前图像的Word格式的结果
